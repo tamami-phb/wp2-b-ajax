3b2996e963f7f7c8ceaef4bd41202cbddc298948
tamami <tamami.oka@gmail.com>
2022-04-07 14:58:45 +0700

    init commit

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+$conn_string = "host=satao.db.elephantsql.com port=5432 " . 
+    "dbname=beajxgfa user=beajxgfa " .
+    "password=kkaCHFWCIBj_PspeTB1WoqeQ3C2n4_0T";
+$conn = pg_connect($conn_string);
+
+// if($conn) {
+//     echo "Koneksi DB Berhasil";
+// } else {
+//     echo "Koneksi DB GAGAL";
+// }
+?>
